7f3abbb722fe195751847c8327b4f8286bd0d32b
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-12-27 16:15:00 +0530

    feat: file upload

--- server/generate/supabase_controllers.py
+++ server/generate/supabase_controllers.py
@@ -6,11 +6,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_ANON_KEY")
+key: str = os.environ.get("SUPABASE_SERVICE_KEY")
 supabase: Client = create_client(url, key)
 
 def downloadVideo(filename:str):
+    filelist = supabase.storage.from_('videos').list()
+    currentFileList = []
     with open('files/' + filename + '.mp4', 'wb+') as f:
-        res = supabase.storage.from_('videos').download(f"{filename}.mp4")
-        f.write(res)
-
+        offset = 0
+        for file in filelist:
+            if filename in file['name']:
+                
+                res = supabase.storage.from_('videos').download(f"{offset}-{filename}.mp4")
+                f.write(res)
+                currentFileList.append(file['name'])
+                offset+=49000000
+    supabase.storage.from_('videos').remove(currentFileList)
