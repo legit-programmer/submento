e7aebcaa6382bbeb651b0f4b913853baea092d0d
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-12-30 15:12:53 +0530

    style: theme

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = ({ supabase }: { supabase: SupabaseClient }) => {
         <header className="top-0 z-40 w-full border-b bg-background">
             <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
                 <div className="flex gap-6 md:gap-10">
-                    <span className="inline-block font-bold">Submento</span>
+                    <span className="inline-block font-bold text-3xl">Submento</span>
                 </div>
 
                 <div className="flex flex-1 items-center justify-end">
